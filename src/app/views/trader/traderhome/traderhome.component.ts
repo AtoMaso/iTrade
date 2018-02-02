39941e4ab07529cdb50b1aeb67550bf88a83d535
AtoMaso <srbinovskimirko@gmail.com>
2018-02-02 23:16:49 +1000

    fixing styling

--- src/app/views/trader/traderhome/traderhome.component.ts
+++ src/app/views/trader/traderhome/traderhome.component.ts
@@ -7,6 +7,7 @@ import { LoggerService } from '../../../services/logger/logger.service';
 import { ProcessMessageService } from '../../../services/processmessage/processmessage.service';
 import { PageTitleService } from '../../../services/pagetitle/pagetitle.service';
 // components
+import { MyTradesListComponent } from '../../trade/mytradeslist/mytradeslist.component';
 import { CapsPipe } from '../../../helpers/pipes';
 import { UserSession, UserIdentity, Authentication, Trade, PageTitle } from '../../../helpers/classes';
 import { SpinnerOneComponent } from '../../controls/spinner/spinnerone.component';
@@ -29,6 +30,28 @@ export class TraderHomeComponent implements OnInit {
     this.pageTitleService.emitPageTitle(new PageTitle("Trader Home"));
     this.messagesService.emitRoute("nill");
 
+  }
+
+  ngAfterViewInit() {
+
+
+    jQuery(document).ready(function () {
+
+      jQuery("#collapseCorrespondence").on("hide.bs.collapse", function () {
+        jQuery(".correspondence").html('<span class="glyphicon glyphicon-plus"></span> My Correspondence');
+      });
+      jQuery("#collapseCorrespondence").on("show.bs.collapse", function () {
+        jQuery(".correspondence").html('<span class="glyphicon glyphicon-minus"></span> My Correspondence');
+      });
+
+      jQuery("#collapseTrades").on("hide.bs.collapse", function () {
+        jQuery(".status").html('<span class="glyphicon glyphicon-plus"></span> My Trades Status');
+      });
+      jQuery("#collapseTrades").on("show.bs.collapse", function () {
+        jQuery(".status").html('<span class="glyphicon glyphicon-minus"></span> My Trades Status');
+      });
+
+    });
   }
 
 }
